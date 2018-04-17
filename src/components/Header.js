b6507bceb9b327bbab0f00124779c1bb083568ca
Andrew Usher <andrewusher00@gmail.com>
2018-04-17 15:14:16 -0500

    Complete header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>Unit Converter</h1>
+      </div>
+    </header>
+  )
+}
