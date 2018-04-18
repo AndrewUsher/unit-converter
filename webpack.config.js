5dfb967dcd0fede1648b79a26a57988423999813
Andrew Usher <andrewusher00@gmail.com>
2018-04-18 11:31:09 -0500

    Implement babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs')
