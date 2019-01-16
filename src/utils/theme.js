d3c4588cd9ca9e085b27eec57ff6685e70db80d1
Andrew Usher <andrewusher00@gmail.com>
2019-01-16 15:13:49 -0600

    feat(Theme): Add styled components theme

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  maxSiteWidth: '1200px',
+  primaryColor: '#6A7EFC',
+  secondaryColor: '#FF5656',
+  textColor: '#494953',
+  bgColor: '#EDF2F6',
+  siteFont: 'Roboto, sans-serif'
+}
+
+export default theme
