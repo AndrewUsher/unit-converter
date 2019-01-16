ae9cd9d5a1ad132e5c925f95e1ac6c0d33a8c3bd
Andrew Usher <andrewusher00@gmail.com>
2019-01-16 15:38:27 -0600

    feat(Common Components): Add common components for max width container and buttons

---
+++ src/components/Common/Button/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import { darken } from 'polished'
+import { Container } from '..'
+
+const StyledButton = styled.button`
+  background-color: ${props => darken(0.2, props.theme.primaryColor)};
+  border: none;
+  color: ${props => props.theme.bgColor};
+  cursor: pointer;
+  font-size: 18px;
+  margin-bottom: 40px;
+  padding: 10px 15px;
+  transition: background-color 400ms, transform 300ms;
+
+  &:hover {
+    background-color: ${props => darken(0.4, props.theme.primaryColor)};
+    color: #f3f3f3;
+    transform: scale(1.1);
+  }
+`
+
+const Button = ({ children, ...rest }) => (
+  <Container padLeft>
+    <StyledButton {...rest}>{children}</StyledButton>
+  </Container>
+)
+
+export default Button
