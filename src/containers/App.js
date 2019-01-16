91038d9a16feb1b1a23699b6ffc6d3a5f292c663
Andrew Usher <andrewusher00@gmail.com>
2019-01-16 15:16:02 -0600

    feat(App): Implement styled components theme provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
+import { ThemeProvider } from 'styled-components'
 import Header from '../components/Header'
 import Converter from './Converter'
 import Footer from '../components/Footer'
+import theme from '../utils/theme'
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <Header />
-        <Converter />
-        <Footer />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Fragment>
+      <Header />
+      <Converter />
+      <Footer />
+    </Fragment>
+  </ThemeProvider>
+)
 
 export default App
