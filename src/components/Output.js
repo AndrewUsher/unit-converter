6d843140d4d02a4d0716372b91db7b123aca69fe
Andrew Usher <andrewusher00@gmail.com>
2019-02-21 16:24:49 -0600

    refactor(Output): Migrate Output component to use styled-components

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import { darken } from 'polished'
+import { Container } from './Common'
+
+const OutputContainer = styled.div`
+  background-color: ${props => darken(0.05, props.theme.bgColor)};
+  box-shadow: 2px 3px 1px rgba(#000, 15%);
+  font-size: 30px;
+  width: 100%;
+  height: 30vh;
+  display: flex;
+  font-family: ${props => props.theme.siteFont};
+  align-items: center;
+  justify-content: center;
+`
 
 export default ({ val, unit }) => {
   return (
-    <div className="output-container">
-      <div className="container">
-        <p>
-          {(val && val.toFixed(2)) || 0} {unit}
-        </p>
-      </div>
-    </div>
+    <Container>
+      <OutputContainer>
+        {(val && val.toFixed(2)) || 0} {unit}
+      </OutputContainer>
+    </Container>
   )
 }
